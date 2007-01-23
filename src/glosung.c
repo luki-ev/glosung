9bf742d1b625de77772192d6be9a030efc7d3ccc
eicke <eicke@f3332887-07eb-4060-9106-8d7b7fb90e17>
2007-01-23 22:11:51 +0000

    update header

--- src/glosung.c
+++ src/glosung.c
@@ -1,5 +1,5 @@
 /* glosung.c
- * Copyright (C) 1999-2006 Eicke Godehardt
+ * Copyright (C) 1999-2007 Eicke Godehardt
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
